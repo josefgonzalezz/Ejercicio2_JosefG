1f7007d4767979a26af9bbfd20a95ffe393b1eb2
josefgonzalezz <jgonzalez50082@ufide.ac.cr>
2025-05-29 19:18:45 -0600

    Practica 2
    Practica 2, tarea Josef Gonzalez Jara

---
+++ Ejercicio2_JosefG/src/main/java/Ejercicio2_JosefG/Ejercicio2JosefGApplication.java
@@ -0,0 +1,13 @@
+package Ejercicio2_JosefG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio2JosefGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio2JosefGApplication.class, args);
+	}
+
+}
---
+++ Ejercicio2_JosefG/src/test/java/Ejercicio2_JosefG/Ejercicio2JosefGApplicationTests.java
@@ -0,0 +1,13 @@
+package Ejercicio2_JosefG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio2JosefGApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
